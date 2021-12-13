eadb1f760447009bd07ebc9afc13726d585b89b4
Artiom Tofan <artiomtofancv@gmail.com>
2021-12-13 15:26:41 +0100

    added more posts

--- pages/posts.js
+++ pages/posts.js
@@ -72,6 +72,69 @@ const Posts = () => {
               </div>
             </div>
           </section>
+
+          <section className='posts__section'>
+            <div className='group__header'>
+              <div className='group__title'>Yesterday</div>
+              <div className='group__date'>Monday, Dec 13, 2021</div>
+            </div>
+            <div className='posts__grid'>
+              <div className='avatar'>
+                <Image
+                  src='/favicon.ico'
+                  width={30}
+                  height={30}
+                  alt='Avatar image'
+                />
+              </div>
+              <div className='posts__card'>
+                <div className='posts__header'>
+                  <BiBell className='post__icon' />
+                  <div className='posts__title'>Art liked your post</div>
+                  <div className='posts__time'>9:00am</div>
+                </div>
+                <div className='posts__content'>
+                  The comment of you and a dog was liked by Art.
+                </div>
+              </div>
+              <div className='avatar'>
+                <Image
+                  src='/favicon.ico'
+                  width={30}
+                  height={30}
+                  alt='Avatar image'
+                />
+              </div>
+              <div className='posts__card'>
+                <div className='posts__header'>
+                  <BiBell className='post__icon' />
+                  <div className='posts__title'>Art liked your comment</div>
+                  <div className='posts__time'>9:00am</div>
+                </div>
+              </div>
+              <div className='avatar'>
+                <Image
+                  src='/favicon.ico'
+                  width={30}
+                  height={30}
+                  alt='Avatar image'
+                />
+              </div>
+              <div className='posts__card'>
+                <div className='posts__header'>
+                  <BiBell className='post__icon' />
+                  <div className='posts__title'>Art liked your comment</div>
+                  <div className='posts__time'>9:00am</div>
+                </div>
+                <div className='posts__content'>
+                  The comment of you and a dog was liked by Art. Lorem, ipsum
+                  dolor sit amet consectetur adipisicing elit. Accusamus sit
+                  doloremque odit magni eos soluta neque sunt, autem maxime
+                  tempore!
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     </BaseLayout>
