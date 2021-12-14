7a707d66776a9d0d4dd35167e8a3150b9fbf954a
Artiom Tofan <artiomtofancv@gmail.com>
2021-12-14 16:02:18 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ const Home = () => {
       className='base-layout__home'
     >
       {!user && <Login />}
-      {user && !loggedUser?.name && <Register />}
+      {user && loggedUser?.name?.length > 0 && <Register />}
       {user && loggedUser && <Redirect to='/profile' />}
     </BaseLayout>
   );
